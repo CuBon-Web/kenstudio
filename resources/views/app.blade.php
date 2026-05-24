49d972dd76aff39785b5b59e88a374726df8d2a5
iip-tuananh <tuananhdinamo1122@gmai.com>
2026-05-24 10:04:58 +0700

    first commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -34,7 +34,7 @@
     </style>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script>
-      window.__ENV__ = window.__ENV || {}; __ENV__.link ='https://kenstudio.azas.vn/';
+      window.__ENV__ = window.__ENV || {}; __ENV__.link ='https://kenvisualworks.com/';
       window.Laravel = {!! 
           json_encode([
               'csrf_token' => csrf_token(),
